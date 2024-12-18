144775ea6de4f2ea72cbb7544f1b2f287987197b
hanslhansl <>
2024-12-18 17:08:03 +0100

    a

---
+++ din3990/__init__.py
@@ -0,0 +1,11 @@
+"""
+Implementiert die DIN 743 zur .
+
+Siehe https://github.com/hanslhansl/din3990.
+"""
+
+
+
+# enable colored console output on windows
+import colorama
+colorama.just_fix_windows_console()
